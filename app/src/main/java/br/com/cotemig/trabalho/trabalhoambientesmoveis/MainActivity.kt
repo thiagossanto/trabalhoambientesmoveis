17510178bfbd693d646063eeb5a454510508913c
Thiago Silvério <102246632+thiagossanto@users.noreply.github.com>
2022-05-29 17:29:15 -0300

    Criando repositorio

---
+++ app/src/main/java/br/com/cotemig/trabalho/trabalhoambientesmoveis/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.cotemig.trabalho.trabalhoambientesmoveis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
