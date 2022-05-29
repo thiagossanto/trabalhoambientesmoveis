48e904299a2a591f41297099af1379ea17942812
Thiago Silvério <102246632+thiagossanto@users.noreply.github.com>
2022-05-29 17:42:32 -0300

    Começando o trabalho

--- app/src/main/java/br/com/cotemig/trabalho/trabalhoambientesmoveis/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/cotemig/trabalho/trabalhoambientesmoveis/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.cotemig.trabalho.trabalhoambientesmoveis
+package br.com.cotemig.trabalho.trabalhoambientesmoveis.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.cotemig.trabalho.trabalhoambientesmoveis.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
