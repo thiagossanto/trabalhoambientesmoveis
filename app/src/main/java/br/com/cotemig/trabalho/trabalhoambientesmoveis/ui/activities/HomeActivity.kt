3ed7b69664707072f6b6f9c5ea943b2a0a157178
Thiago Silvério <102246632+thiagossanto@users.noreply.github.com>
2022-05-29 23:20:01 -0300

    Home_page feita

--- app/src/main/java/br/com/cotemig/trabalho/trabalhoambientesmoveis/ui/activities/HomeActivity.kt
+++ app/src/main/java/br/com/cotemig/trabalho/trabalhoambientesmoveis/ui/activities/HomeActivity.kt
@@ -1,27 +1,19 @@
 package br.com.cotemig.trabalho.trabalhoambientesmoveis.ui.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.WindowManager
 import br.com.cotemig.trabalho.trabalhoambientesmoveis.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-
-        Handler().postDelayed({
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
 
     }
 }
