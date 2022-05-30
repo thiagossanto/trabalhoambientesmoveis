841f24b65a5387fb1884f06ab2974bf932cdc011
Thiago Silvério <102246632+thiagossanto@users.noreply.github.com>
2022-05-30 02:20:28 -0300

    telas de cadastro completas

--- app/src/main/java/br/com/cotemig/trabalho/trabalhoambientesmoveis/ui/activities/ForgotActivity.kt
+++ app/src/main/java/br/com/cotemig/trabalho/trabalhoambientesmoveis/ui/activities/ForgotActivity.kt
@@ -4,29 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import android.widget.Button
+import android.widget.ImageButton
 import br.com.cotemig.trabalho.trabalhoambientesmoveis.R
 
-class HomeActivity : AppCompatActivity() {
+class ForgotActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_forgot)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        var btnForgot = findViewById<Button>(R.id.btnForgot)
-        btnForgot.setOnClickListener {
-            var intent = Intent(this, ForgotActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        var btnRegister = findViewById<Button>(R.id.btnRegister)
-        btnRegister.setOnClickListener {
-            var intent = Intent(this, RegisterActivity::class.java)
+        var backIcon = findViewById<ImageButton>(R.id.backIcon)
+        backIcon.setOnClickListener {
+            var intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
         }
