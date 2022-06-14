69179c8a111cc20268e779b0878672487f0b37fa
Alexandre <alexandresouzacamargo@gmail.com>
2022-06-14 18:18:34 -0300

    aparentemente acabou

--- app/src/main/java/br/com/cotemig/trabalho/trabalhoambientesmoveis/ui/activities/InfoActivity.kt
+++ app/src/main/java/br/com/cotemig/trabalho/trabalhoambientesmoveis/ui/activities/InfoActivity.kt
@@ -3,6 +3,7 @@ package br.com.cotemig.trabalho.trabalhoambientesmoveis.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.ImageButton
 import br.com.cotemig.trabalho.trabalhoambientesmoveis.R
 
@@ -11,7 +12,12 @@ class InfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
-        var backIcon = findViewById<ImageButton>(R.id.backIcon)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        var backIcon = findViewById<ImageButton>(R.id.imageVoltar)
         backIcon.setOnClickListener {
             var intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
